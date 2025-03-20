34f5ea421555f267c88044c12a18281b939d4b92
krish <krisharora338@gamil.com>
2025-03-20 10:57:32 +0530

    hood degine done

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     
-$(',text').textillate({
-    loop: true,
-    sync: true,
-    in:{
-        effect: "bounceIn",
-    },
-    out:{
-        effect: "bounceOut",
-    },
-});
-
-
-});
+    $(',text').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "bounceIn",
+        },
+        out:{
+            effect: "bounceOut",
+        },
+    });
+    
+    
+    });
